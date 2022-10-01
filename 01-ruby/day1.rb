dc1bbeda384511eef71a4f21d8f01f3e53da41ea
Alexey Ermakov <strangebrew@yandex.ru>
2022-10-01 15:24:32 +0300

    Ruby day 1

---
+++ 01-ruby/day1.rb
@@ -0,0 +1,15 @@
+puts 'Hello, world'
+
+'Hello, Ruby' =~ /Ruby/
+
+x = 10
+until x == 0
+    puts 'Alexey'
+    x = x - 1
+end
+
+x = 1
+while x <= 10
+    puts "This is sentence number #{x}"
+    x = x + 1
+end
